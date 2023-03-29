3efc040fad098791e662260967f8b9297b6feef4
Everton da Fonseca Jardim Junior <evertonjunior2001@hotmail.com>
2023-03-29 06:15:36 -0300

    Update ex017.py

--- ex017.py
+++ ex017.py
@@ -1,5 +1,7 @@
 cob_tinta = 6
 tam_lata = 18
 preco_lata = 80
-tam_galao = 
-preco_galao =
+tam_galao = 3.6
+preco_galao = 25
+tam_met = float(input('Qual o tamanho em metros quadrados a área deve ser pintada? '))
+tinta = tam_met / cob_tinta
